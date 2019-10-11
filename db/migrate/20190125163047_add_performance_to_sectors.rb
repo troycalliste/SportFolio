330814967c298a7ff4f5699abbbccfc81a8d00ea
troycalliste <troycallist@gmail.com>
2019-10-11 11:36:17 -0400

    rm5

--- db/migrate/20190125163047_add_performance_to_sectors.rb
+++ db/migrate/20190125163047_add_performance_to_sectors.rb
@@ -1,5 +1,5 @@
 class AddPerformanceToSectors < ActiveRecord::Migration[5.1]
   def change
-    add_column :sectors, :performance, :string
+    # add_column :sectors, :performance, :string
   end
 end
